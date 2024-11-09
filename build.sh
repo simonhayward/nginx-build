85a280b6a71ea42b921e1ef604529511b5dfaa12
Simon Hayward <simonhayward@users.noreply.github.com>
2024-11-09 12:43:34 +0000

    Modsecurity (#2)
    * init modsecurity

--- build.sh
+++ build.sh
@@ -8,6 +8,19 @@ NGINX_GROUP=nginx
 
 mkdir -p ${NGINX_SRC}
 
+# modsecurity
+cd ${NGINX_SRC}
+git clone https://github.com/owasp-modsecurity/ModSecurity
+cd ModSecurity/
+git submodule init
+git submodule update
+sh build.sh
+./configure
+make && sudo make install
+
+cd ${NGINX_SRC}
+git clone --depth 1 https://github.com/owasp-modsecurity/ModSecurity-nginx.git
+
 # pagespeed
 cd ${NGINX_SRC}
 wget https://github.com/apache/incubator-pagespeed-ngx/archive/${NPS_COMMIT}.zip
@@ -23,5 +36,5 @@ cd ${NGINX_SRC}
 wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 tar -xvzf nginx-${NGINX_VERSION}.tar.gz
 cd nginx-${NGINX_VERSION}
-./configure --prefix=${NGINX_DIR}/compiled/${NGINX_VERSION} --user=${NGINX_USER} --group=${NGINX_GROUP} --with-http_ssl_module --with-http_stub_status_module --add-module=${NGINX_SRC}/${NPS_DIR} --with-http_v2_module --with-http_v3_module --with-debug --with-http_realip_module
+./configure --prefix=${NGINX_DIR}/compiled/${NGINX_VERSION} --user=${NGINX_USER} --group=${NGINX_GROUP} --with-http_ssl_module --with-http_stub_status_module --add-module=${NGINX_SRC}/${NPS_DIR} --with-http_v2_module --with-http_v3_module --with-debug --with-http_realip_module --with-compat --add-dynamic-module=${NGINX_SRC}/ModSecurity-nginx
 make && make install
